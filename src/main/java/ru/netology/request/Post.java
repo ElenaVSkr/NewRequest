efcfad464af81158b36de4abb6f5a178bea9915d
Elena S <prostovael@yandex.ru>
2024-03-14 22:25:18 +0400

    src/

---
+++ src/main/java/ru/netology/request/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.request;
+
+public class Post {
+    public String name; // поле для имени
+    public String patronymic; // поле для отчества
+    public String surname; // поле для фамилии
+    public String phone; // поле для ввода телефона
+    public String passport; // поле для ввода паспорта
+
+    public boolean subscription;
+    public FormDate birthday;
+}
